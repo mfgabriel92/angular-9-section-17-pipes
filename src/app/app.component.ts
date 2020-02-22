65e6d1c2e292146af709498e7f13fe23e0b5d211
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-21 22:02:03 -0300

    Example: Creating a Filter Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  filterStatus: string;
   servers = [
     {
       instanceType: 'medium',
